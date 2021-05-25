69d14773cf790f4f426136c3e5bcc864a099c61f
Ze Yu <yuze@iphy.ac.cn>
2021-05-25 12:55:15 +0800

    RingArray

--- Algorithm/LinkedLists/RingArray.java
+++ Algorithm/LinkedLists/RingArray.java
@@ -5,8 +5,11 @@ public class RingArray {
 
     }
 
+    // 使用数组实现 队列： 先进先出
+    // 额外使用一个变量 size 可以省略复杂的双指针追赶的问题
+    // 极大地简化了coding 难度
     public static class MyQueue {
-        private int[] arr;
+        private final int[] arr;
         private int pushi;
         private int polli;
         private int size;
@@ -42,6 +45,36 @@ public class RingArray {
 
         public int nextIndex(int index){
             return index < limit - 1? index+1 : 0;
+        }
+    }
+
+    // 使用数组实现 栈： 先进后出
+    // 相比于队列的实现就简单了很多
+    // 只需要使用一个指针
+    public static class MyStack {
+        private final int[] arr;
+        private int pushi;
+
+        public MyStack(int limit){
+            arr = new int[limit];
+            pushi = 0;
+        }
+
+        public void push(int value){
+            if (pushi == 7){
+                throw new RuntimeException("栈满了");
+            }
+
+            arr[pushi] = value;
+            pushi++;
+        }
+
+        public int pop(){
+            if (pushi == 0){
+                throw new RuntimeException("栈空了");
+            }
+            pushi--;
+            return arr[pushi];
         }
     }
 
